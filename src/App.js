998051915af888b9f907491574dae15ee1db9a62
lucky-xz <1348521764@qq.com>
2020-12-14 15:03:36 +0800

    react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  let name = '张三'
+  let age = 20
+  return (
+    <div className="App">
+      <div>姓名：{name}</div>
+      <p>年龄：{age}</p>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';  //解析jsx语法糖
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
+
